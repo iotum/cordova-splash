ed3077fc6965315c3e433084fa9bde042bc72c5a
Rex Chiu <rex@iotum.com>
2020-11-12 15:25:28 -0500

    remove iOS and Windows platforms

--- index.js
+++ index.js
@@ -31,32 +31,6 @@ var getPlatforms = function (projectName) {
   }
 
   platforms.push({
-    name : 'ios',
-    // TODO: use async fs.exists
-    isAdded : fs.existsSync('platforms/ios'),
-    splashPath : 'platforms/ios/' + projectName + xcodeFolder,
-    splash : [
-      // iPhone
-      { name: 'Default~iphone.png',            width: 320,  height: 480  },
-      { name: 'Default@2x~iphone.png',         width: 640,  height: 960  },
-      { name: 'Default-568h@2x~iphone.png',    width: 640,  height: 1136 },
-      { name: 'Default-667h.png',              width: 750,  height: 1334 },
-      { name: 'Default-736h.png',              width: 1242, height: 2208 },
-      { name: 'Default-Landscape-736h.png',    width: 2208, height: 1242 },
-      { name: 'Default-2436h.png',             width: 1125, height: 2436 },
-      { name: 'Default-Landscape-2436h.png',   width: 2436, height: 1125 },
-      { name: 'Default-2688h.png',             width: 1242, height: 2688 },
-      { name: 'Default-Landscape-2688h.png',   width: 2688, height: 1242 },
-      { name: 'Default-1792h.png',             width: 828, height: 1792 },
-      { name: 'Default-Landscape-1792h.png',   width: 1792, height: 828 },
-      // iPad
-      { name: 'Default-Portrait~ipad.png',     width: 768,  height: 1024 },
-      { name: 'Default-Portrait@2x~ipad.png',  width: 1536, height: 2048 },
-      { name: 'Default-Landscape~ipad.png',    width: 1024, height: 768  },
-      { name: 'Default-Landscape@2x~ipad.png', width: 2048, height: 1536 }
-    ]
-  });
-  platforms.push({
     name : 'android',
     isAdded : fs.existsSync('platforms/android'),
     splashPath : 'platforms/android/res/',
@@ -75,25 +49,6 @@ var getPlatforms = function (projectName) {
       { name: 'drawable-port-xhdpi/screen.png', width: 720,  height: 1280 },
       { name: 'drawable-port-xxhdpi/screen.png', width: 960, height: 1600  },
       { name: 'drawable-port-xxxhdpi/screen.png', width: 1280, height: 1920  }
-    ]
-  });
-  platforms.push({
-    name : 'windows',
-    isAdded : fs.existsSync('platforms/windows'),
-    splashPath : 'platforms/windows/images/',
-    splash : [
-      // Landscape
-      { name: 'SplashScreen.scale-100.png', width: 620,  height: 300  },
-      { name: 'SplashScreen.scale-125.png', width: 775,  height: 375  },
-      { name: 'SplashScreen.scale-140.png', width: 868,  height: 420  },
-      { name: 'SplashScreen.scale-150.png', width: 930,  height: 450  },
-      { name: 'SplashScreen.scale-180.png', width: 1116,  height: 540  },
-      { name: 'SplashScreen.scale-200.png', width: 1240, height: 600  },
-      { name: 'SplashScreen.scale-400.png', width: 2480, height: 1200 },
-      // Portrait
-      { name: 'SplashScreenPhone.scale-240.png', width: 1152,  height: 1920  },
-      { name: 'SplashScreenPhone.scale-140.png', width: 672,  height: 1120  },
-      { name: 'SplashScreenPhone.scale-100.png', width: 480,  height: 800  }
     ]
   });
   deferred.resolve(platforms);
